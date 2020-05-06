02c4fcd51ee63ddb5fb0b7e48c0954d21547c049
Adam <adam.kjall@hotmail.com>
2020-05-06 17:29:56 +0200

    cors img-src test

--- webpack.config.js
+++ webpack.config.js
@@ -139,7 +139,11 @@ export default {
           'https://google.com/images',
           'http://localhost:3000/favicon.ico',
           'https://*.googleusercontent.com',
+          'https://lh1.googleusercontent.com',
+          'https://lh2.googleusercontent.com',
+          'https://lh3.googleusercontent.com',
           'https://lh4.googleusercontent.com',
+          'https://lh5.googleusercontent.com',
           'https://firestore.googleapis.com',
           'https://firebasestorage.googleapis.com'
         ],

efcec8a8635d20f545313a9264a482e298c1fda0
Åsa Palholmen <asa.palholmen@humblebee.se>
2020-05-12 16:07:20 +0200

    some minor changes in Sign up with google

--- src/components/SignUpWithGoogle/index.jsx
+++ src/components/SignUpWithGoogle/index.jsx
@@ -5,30 +5,26 @@ import { updateUserProfileDocument } from '../../firebase/firebase.utils';
 import { AuthenticationContext } from '../../contexts/AuthenticationContext';
 import UploadFile from '../../components/UploadFile';
 
+import BackButton from '../BackButton';
 import Button from '../UI/Button';
 import H1 from '../UI/H1';
 import Input from '../UI/Input';
 import isValidDate from '../../helpers/functions/validDate';
 import makeStringtoBirthDate from '../../helpers/functions/makeStringtoBirthDate';
+import graywaves from '../../assets/icons/graywaves.svg';
 
 import { StyledContainer } from './style';
 
 const SignUpWithGoogle = () => {
   const { user } = useContext(AuthenticationContext);
   const history = useHistory();
-  const [msgName, setMsgName] = useState('');
   const [msgGender, setGender] = useState('');
   const [msgBirth, setMsgBirth] = useState('');
   const [msg, setMsg] = useState('');
   const [msglvl, setMsglvl] = useState('');
   const [toogle, setToogle] = useState(false);
 
-  const toogleChangepic = e => {
-    setToogle(!toogle);
-  };
-
   const [inputs, setInputs] = useState({
-    username: user.displayName || '',
     dateOfBirth: '',
     lvlOfSwedish: '',
     gender: ''
@@ -37,9 +33,6 @@ const SignUpWithGoogle = () => {
   const onSubmit = async event => {
     event.preventDefault();
 
-    if (inputs.username.length <= 1) {
-      setMsgName('Fyll i Ditt namn.');
-    }
     if (isValidDate(inputs.dateOfBirth) == 'Not valid date') {
       setMsgBirth('Fyll i det datum du är född.');
     }
@@ -51,7 +44,6 @@ const SignUpWithGoogle = () => {
     } else {
       try {
         updateUserProfileDocument(user.id, {
-          displayName: inputs.username,
           dateOfBirth: makeStringtoBirthDate(inputs.dateOfBirth),
           lvlOfSwedish: inputs.lvlOfSwedish,
           gender: inputs.gender
@@ -74,112 +66,136 @@ const SignUpWithGoogle = () => {
 
   return (
     <StyledContainer>
-      <form onSubmit={onSubmit}>
+      <div className="headcontainer">
+        <BackButton />
         <H1>Skapa konto</H1>
-        <p className="red">{msgName}</p>
-        <Input
-          type="text"
-          autoComplete="name"
-          label="Förnamn och efternamn *"
-          id="username"
-          inline
-          name="username"
-          placeholder="Namn Efternamn"
-          value={inputs.username}
-          onChange={event => onValueChange('username', event.target.value)}
-        />
+      </div>
+      <img src={graywaves} className="waves" alt="wave" />
+      <form onSubmit={onSubmit}>
+        <p className="moreinfo">Vi behöver ytterligare information innan ditt konto är klart.</p>
+        <div className="signup-form-container">
+          {inputs.dateOfBirth.length !== 8 ? (
+            <div className="reddott" />
+          ) : (
+            <div className="donedott" />
+          )}
+          <Input
+            type="text"
+            autoComplete="bday"
+            label="Födelsedatum *"
+            id="dateOfBirth"
+            inline
+            name="dateOfBirth"
+            placeholder="ÅÅÅÅMMDD"
+            value={inputs.dateOfBirth}
+            onChange={event => onValueChange('dateOfBirth', event.target.value)}
+          />
+          <div className="redline nr1" />
+          <p className="red">{msgBirth}</p>
+          {inputs.lvlOfSwedish.length <= 1 ? (
+            <div className="reddott nr2" />
+          ) : (
+            <div className="donedott nr2" />
+          )}
+          <div className="swedish">
+            <p>Hur är din svenskanivå? *</p>
+            <label htmlFor="newSwede">
+              <input
+                type="radio"
+                id="newSwede"
+                name="swedelvl"
+                value="Jag vill lära mig mer svenska"
+                onChange={event => onValueChange('lvlOfSwedish', event.target.value)}
+              />
+              Jag vill lära mig mer svenska
+            </label>
+            <br />
+            <label htmlFor="establish">
+              <input
+                type="radio"
+                id="establish"
+                name="swedelvl"
+                value="Jag pratar flytande svenska"
+                onChange={event => onValueChange('lvlOfSwedish', event.target.value)}
+              />
+              Jag pratar flytande svenska{' '}
+            </label>
+          </div>
+          <div className="redline nr2" />
+          <p className="red">{msglvl}</p>
 
-        <p className="red">{msgBirth}</p>
-        <Input
-          type="text"
-          autoComplete="bday"
-          label="Födelsedatum *"
-          id="dateOfBirth"
-          inline
-          name="dateOfBirth"
-          placeholder="ÅÅÅÅMMDD"
-          value={inputs.dateOfBirth}
-          onChange={event => onValueChange('dateOfBirth', event.target.value)}
-        />
-        <p className="red">{msglvl}</p>
-        <div className="swedish">
-          <p>Är du ny eller etablerad? *</p>
-          <label htmlFor="newSwede">
-            <input
-              type="radio"
-              id="newSwede"
-              name="swedelvl"
-              value="Ny Svensk"
-              onChange={event => onValueChange('lvlOfSwedish', event.target.value)}
-            />
-            Ny svensk - jag vill bli bättre på svenska
-          </label>
-          <br />
-          <label htmlFor="establish">
-            <input
-              type="radio"
-              id="establish"
-              name="swedelvl"
-              value="Etablerad svensk"
-              onChange={event => onValueChange('lvlOfSwedish', event.target.value)}
-            />
-            Etablerad svensk - jag pratar flytande svenska.
-          </label>
-        </div>
-        <p className="red">{msgGender}</p>
-        <div className="gender">
-          <p>Kön? *</p>
-          <label htmlFor="female">
-            <input
-              type="radio"
-              id="female"
-              name="gender"
-              value="female"
-              onChange={event => onValueChange('gender', event.target.value)}
-            />
-            Kvinna
-          </label>
-          <label htmlFor="male">
-            <input
-              type="radio"
-              id="male"
-              name="gender"
-              value="male"
-              onChange={event => onValueChange('gender', event.target.value)}
-            />
-            Man
-          </label>
-          <label htmlFor="other">
-            <input
-              type="radio"
-              id="other"
-              name="gender"
-              value="other"
-              onChange={event => onValueChange('gender', event.target.value)}
-            />
-            Annat / vill inte svara
-          </label>
-        </div>
-        <div className="profilebox-1">
-          <img className="avatar" src={user.photoUrl} alt="avatar" />
-          <span
-            className="changepic"
-            tabIndex="-5"
-            role="button"
-            aria-label="toogle"
-            onClick={toogleChangepic}
-            onKeyDown={toogleChangepic}
-          >
-            {toogle ? 'Klicka här efter laddat upp din nya bild' : 'Byt bild'}
-          </span>
-        </div>
-        <p className="red"> {msg} </p>
+          {inputs.gender.length <= 0 ? (
+            <div className="reddott nr3" />
+          ) : (
+            <div className="donedott nr3" />
+          )}
 
-        <p>* Obligatoriska fält</p>
+          <div className="gender">
+            <p>Kön? *</p>
+            <label htmlFor="female">
+              <input
+                type="radio"
+                id="female"
+                name="gender"
+                value="female"
+                onChange={event => onValueChange('gender', event.target.value)}
+              />
+              Kvinna
+            </label>
+            <label htmlFor="male">
+              <input
+                type="radio"
+                id="male"
+                name="gender"
+                value="male"
+                onChange={event => onValueChange('gender', event.target.value)}
+              />
+              Man
+            </label>
+            <label htmlFor="other">
+              <input
+                type="radio"
+                id="other"
+                name="gender"
+                value="other"
+                onChange={event => onValueChange('gender', event.target.value)}
+              />
+              Vill inte ange
+            </label>
+          </div>
+          <div className="redline nr3" />
+          <p className="red">{msgGender}</p>
+          {inputs.gender.length <= 0 ? <div className="reddott" /> : <div className="donedott" />}
+          <div className="profilebox-1">
+            <img className="avatar" src={user.photoUrl} alt="avatar" />
+            <span
+              className="changepic"
+              tabIndex="-5"
+              role="button"
+              aria-label="toogle"
+              onClick={() => {
+                setToogle(!toogle);
+              }}
+              onKeyDown={() => {
+                setToogle(!toogle);
+              }}
+            >
+              {toogle ? 'Klicka här efter laddat upp din nya bild' : 'Byt bild'}
+            </span>
+          </div>
+          <div className="redline nr3" />
+          <p className="red"> {msg} </p>
+          {inputs.gender.length <= 0 ? <div className="reddott" /> : <div className="donedott" />}
+          <p>Jag godkänner allmänna villkoren</p>
+          <div />
+          <p>* Obligatoriska fält</p>
+        </div>
         {toogle ? null : (
-          <Button nature="default" stretch type="submit">
-            Skapa konto
-          </Button>
+          <div className="buttondiv">
+            <Button nature="default" className="nextbutton" stretch type="submit">
+              Skapa konto
+            </Button>
+          </div>
         )}
       </form>
 
--- src/components/SignUpWithGoogle/style.jsx
+++ src/components/SignUpWithGoogle/style.jsx
@@ -3,12 +3,44 @@ import fontSizes from 'tokens/fontSizes.mjs';
 import colors from 'tokens/colors.mjs';
 
 export const StyledContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  .headcontainer {
+    background-color: ${colors.gray5};
+    display: grid;
+    grid-template-columns: 50px 8fr;
+    h1 {
+      padding-top: 1rem;
+    }
+    margin: 0;
+  }
+
+  .waves {
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    position: relative;
+    top: -5px;
+  }
+
+  .moreinfo {
+    margin: 1.5rem 3rem;
+  }
+
+  .signup-form-container {
+    margin: 1rem auto 0;
+    max-width: 315px;
+    display: grid;
+    grid-template-columns: 50px 8fr;
+    grid-template-rows: 0.5fr 1px 1fr 1px 0.7fr 1px 0.3fr 1px 0.3fr 1px 0.6fr;
+  }
 
   .red {
+    font-size: ${fontSizes.microcopy};
+    position: relative;
+    top: 0px;
+    left: 10px;
     color: red;
+    height: 1px;
+    margin: 0;
   }
 
   label {
@@ -31,6 +63,7 @@ export const StyledContainer = styled.div`
     }
   }
   .avatar {
+    margin: 1rem;
     width: 92px;
     height: 92px;
     border-radius: 999rem;
@@ -45,5 +78,51 @@ export const StyledContainer = styled.div`
     font-size: ${fontSizes.s};
     text-align: center;
     border: none;
+  }
+
+  .reddott {
+    position: relative;
+    left: 15px;
+    top: 35px;
+    width: 15px;
+    height: 15px;
+    border-radius: 999rem;
+    border: 3px ${colors.red} solid;
+    background-color: white;
+    &.nr2 {
+      top: 15px;
+    }
+  }
+
+  .donedott {
+    position: relative;
+    left: 15px;
+    top: 35px;
+    width: 15px;
+    height: 15px;
+    border-radius: 999rem;
+    border: 3px ${colors.red} solid;
+    background-color: ${colors.red};
+    &.nr2 {
+      top: 15px;
+    }
+  }
+
+  .redline {
+    width: 1px;
+    height: 40px;
+    background-color: ${colors.red};
+    position: relative;
+    left: 21px;
+    top: -25px;
+  }
+
+  .buttondiv {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    .nextbutton {
+      margin: 0 1rem;
+    }
   }
 `;

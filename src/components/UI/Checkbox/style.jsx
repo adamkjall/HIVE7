67862f711521105bad2081d2e783a28bcf22c1fd
Åsa Palholmen <asa.palholmen@humblebee.se>
2020-05-25 17:18:06 +0200

    radiobutton and checkbox fix

--- src/components/UI/Checkbox/style.jsx
+++ src/components/UI/Checkbox/style.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import spacing from 'tokens/spacing.mjs';
 import colors from 'tokens/colors.mjs';
 import fontSizes from 'tokens/fontSizes.mjs';
-
+import bock from '../../../assets/icons/bock.svg';
 export const StyledCheckBox = styled.div`
   font-size: ${fontSizes.m};
   max-height: 50px;
@@ -35,27 +35,30 @@ export const StyledCheckBox = styled.div`
     height: 20px;
     width: 20px;
     content: '';
-    appearance: none;
-    outline: none;
+    appearance: none !important;
+    outline: none !important;
     &:before {
       content: '';
-      width: 19px;
-      height: 19px;
+      width: 20px;
+      height: 20px;
+      outline: none !important;
     }
 
     &:checked {
       background-color: ${colors.red};
-      content: url("data:image/svg+xml;charset=UTF-8, <svg width='18' height='13' viewBox='0 0 18 13' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M16.5 1L6.5 11L1 6.5' stroke='white' stroke-width='1.5'/></svg>");
+      content: '';
+      background-image: url(${bock});
       background-size: contain;
       background-position: center;
       background-repeat: no-repeat;
+      border: 2px ${colors.red} solid;
       width: 23px;
       height: 23px;
       position: relative;
       top: -2px;
       left: -2px;
       padding: 2px;
-      outline: none;
+      outline: none !important;
       border-radius: 3px;
     }
   }
